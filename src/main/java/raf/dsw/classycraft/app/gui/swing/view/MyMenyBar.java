a4933375aad9b5861dc56db7829d28de675cc158
alsako <akovacevic12422rn@raf.rs>
2024-01-08 22:58:17 +0100

    Dodate akcije na FileMenu i EditMenu

--- src/main/java/raf/dsw/classycraft/app/gui/swing/view/MyMenyBar.java
+++ src/main/java/raf/dsw/classycraft/app/gui/swing/view/MyMenyBar.java
@@ -8,16 +8,33 @@ public class MyMenyBar extends JMenuBar {
     public MyMenyBar(){
         JMenu fileMenu = new JMenu("File");
         fileMenu.setMnemonic(KeyEvent.VK_F);
-        fileMenu.add(MainFrame.getInstance().getActionManager().getExitAction());
-        fileMenu.add(MainFrame.getInstance().getActionManager().getNewProjectAction());
-        fileMenu.add(MainFrame.getInstance().getActionManager().getDeleteNodeAction());
-        fileMenu.add(MainFrame.getInstance().getActionManager().getSetAuthorAction());
+        fileMenu.add(new JMenuItem(MainFrame.getInstance().getActionManager().getExitAction()));
+        fileMenu.add(new JMenuItem(MainFrame.getInstance().getActionManager().getNewProjectAction()));
+        fileMenu.add(new JMenuItem(MainFrame.getInstance().getActionManager().getDeleteNodeAction()));
+        fileMenu.add(new JMenuItem(MainFrame.getInstance().getActionManager().getOpenProjectAction()));
+        fileMenu.add(new JMenuItem(MainFrame.getInstance().getActionManager().getLoadDiagramAction()));
+        fileMenu.add(new JMenuItem(MainFrame.getInstance().getActionManager().getSaveAction()));
+        fileMenu.add(new JMenuItem(MainFrame.getInstance().getActionManager().getExportPhotoAction()));
 
+        for (int i=0; i<fileMenu.getItemCount();i++){
+            JMenuItem item = fileMenu.getItem(i);
+            item.setIcon(null);
+        }
 
         add(fileMenu);
 
         JMenu editMenu = new JMenu("Edit");
-        editMenu.add(MainFrame.getInstance().getActionManager().getAboutUsAction());
+        editMenu.setMnemonic(KeyEvent.VK_E);
+        editMenu.add(new JMenuItem(MainFrame.getInstance().getActionManager().getUndoAction()));
+        editMenu.add(new JMenuItem(MainFrame.getInstance().getActionManager().getRedoAction()));
+        editMenu.add(new JMenuItem(MainFrame.getInstance().getActionManager().getSetAuthorAction()));
+        editMenu.add(new JMenuItem(MainFrame.getInstance().getActionManager().getAboutUsAction()));
+
+        for (int i=0; i<editMenu.getItemCount();i++){
+            JMenuItem item = editMenu.getItem(i);
+            item.setIcon(null);
+        }
+
         add(editMenu);
     }
 
